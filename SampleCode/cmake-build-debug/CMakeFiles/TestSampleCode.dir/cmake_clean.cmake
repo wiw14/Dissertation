c8a2db3973e9e949548ca24a99b9db31c5d6f224
wiw14 <74732052+wiw14@users.noreply.github.com>
2021-01-31 20:11:10 +0000

    Adding benchmark datasets and Sample code from the competition

---
+++ SampleCode/cmake-build-debug/CMakeFiles/TestSampleCode.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestSampleCode.dir/EVRP.cpp.o"
+  "CMakeFiles/TestSampleCode.dir/heuristic.cpp.o"
+  "CMakeFiles/TestSampleCode.dir/main.cpp.o"
+  "CMakeFiles/TestSampleCode.dir/stats.cpp.o"
+  "TestSampleCode.exe"
+  "TestSampleCode.pdb"
+  "libTestSampleCode.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestSampleCode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
